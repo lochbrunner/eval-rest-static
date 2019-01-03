5ef72ead39dc75d989b90865f8cb16a0f451eee5
Matthias Lochbrunner <matthias_lochbrunner@web.de>
2019-01-03 16:56:20 +0100

    First attempt

--- app.go
+++ app.go
@@ -9,26 +9,54 @@ import (
 	"github.com/gorilla/mux"
 )
 
+// Data is the container for some example data
 type Data struct {
 	Value string `json:"value,omitempty"`
+}
+
+// AddRequest contains the request data for the add command
+type AddRequest struct {
+	A int `json:"a,omitempty"`
+	B int `json:"b,omitempty"`
+}
+
+// AddResponse contains the response data for the add command
+type AddResponse struct {
+	Result int `json:"result,omitempty"`
 }
 
 var data []Data
 
 func getData(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(data)
 }
 
+func add(w http.ResponseWriter, r *http.Request) {
+	decoder := json.NewDecoder(r.Body)
+	var t AddRequest
+	err := decoder.Decode(&t)
+	if err != nil {
+		panic(err)
+	}
+	var res AddResponse
+	res.Result = t.A + t.B
+	w.WriteHeader(http.StatusOK)
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(res)
+}
+
 func apiRouter() *mux.Router {
-	router := mux.NewRouter()
-	router = mux.NewRouter().StrictSlash(true)
+	router := mux.NewRouter().StrictSlash(true)
 	router.HandleFunc("/api/data", getData).Methods("GET")
+	router.HandleFunc("/api/add", add).Methods("POST")
 	return router
 }
 
 func main() {
 
-	data = append(data, Data{Value: "Hi"})
+	data = append(data, Data{Value: "Hi there!"})
 
 	router := apiRouter()
 	http.Handle("/api/", router)
